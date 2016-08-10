0dd53cfb5c5c010d1572f27ec4056015553a91eb
anon <gitdev@web.de>
2016-08-10 23:09:06 +0200

    reading / writing of webconten works now

--- NeuralWeatherForecastOptimization/NeuralWeatherForecastOptimization.pro
+++ NeuralWeatherForecastOptimization/NeuralWeatherForecastOptimization.pro
@@ -9,10 +9,42 @@ QT       += core
 QT       -= gui
 
 TARGET = NeuralWeatherForecastOptimization
+
 CONFIG   += console
 CONFIG   -= app_bundle
+CONFIG   += c++11
 
 TEMPLATE = app
 
+INCLUDEPATH += include/
 
-SOURCES += main.cpp
+LIBS += -lcurl
+
+
+SOURCES += main.cpp \
+    src/airpressuresensor.cpp \
+    src/sensor.cpp \
+    src/temperaturesensor.cpp \
+    src/DataBuffer.cpp \
+    src/dbinterface.cpp \
+    src/HTTPRequest.cpp \
+    src/logwriter.cpp \
+    src/slevel.cpp \
+    src/OpenWeatherMap.cpp \
+    src/WebContent.cpp
+
+HEADERS += \
+    include/airpressuresensor.h \
+    include/sensor.h \
+    include/sensortype.h \
+    include/temperaturesensor.h \
+    include/catch.hpp \
+    include/config.h \
+    include/DataBuffer.h \
+    include/dbinterface.h \
+    include/HTTPRequest.h \
+    include/logwriter.h \
+    include/slevel.h \
+    include/OpenWeatherMap.h \
+    include/WebContent.h \
+    include/WebContentType.h
--- NeuralWeatherForecastOptimization/main.cpp
+++ NeuralWeatherForecastOptimization/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+// ----
+#include "DataBuffer.h"
+#include "sensor.h"
+#include "temperaturesensor.h"
+#include "airpressuresensor.h"
+#include "dbinterface.h"
+#include "logwriter.h"
+#include "WebContent.h"
+#include "OpenWeatherMap.h"
 
 
 /**
@@ -7,6 +16,64 @@
  *
  */
 int main() {
+    // create Log-Writer Object
+    LogWriter log("DBWriterDaemon", "Log.txt");
+
+    // create and init singleton-DBInterface-object
+    DBInterface& dbi = DBInterface::getInstance();
+    // todo
+    dbi.writeStatusOK(true);
+    dbi.init();
+
+    // create sensor objects
+    Sensor AirTemperature2MSensor;
+    AirTemperature2MSensor.setSensorType(new TemperatureSensor());
+    Sensor AirPressure2MSensor;
+    AirPressure2MSensor.setSensorType(new AirPressureSensor());
+
+    // read sensor values
+    DataBuffer SensorValuesBuffer;
+    SensorValuesBuffer.dataSource = "WeatherStation";
+    SensorValuesBuffer.useDataSource = true;
+    SensorValuesBuffer.data["Lufttemperatur_2m"] = AirTemperature2MSensor.readSensor();
+    SensorValuesBuffer.data["Luftdruck_2m"]      = AirPressure2MSensor.readSensor();
+
+    // create webcontent - objects
+    WebContent OpenWeatherMapForecast;
+    OpenWeatherMapForecast.setWebContentType(new OpenWeatherMap());
+
+    // read forecast values
+    DataBuffer ForecastValuesBuffer;
+    ForecastValuesBuffer.dataSource = "Forecast";
+    ForecastValuesBuffer = OpenWeatherMapForecast.readWebContent();
+
+    // check status
+    bool statusOK = dbi.readStatusOK();
+    if (!statusOK) {
+        log << SLevel(ERROR) << "Aborted writing sensor-data and forecast-data to" <<
+                                " database, because of status is not okay." << endl;
+    } else {
+        // write sensor values to database
+        dbi.writeToDataBase(SensorValuesBuffer);
+        bool dbFailure = dbi.getDBFailure();
+        if (!dbFailure) {
+            log << SLevel(INFO) << "Succesfully wrote sensor-data to database." << endl;
+        } else {
+            log << SLevel(ERROR) << "DB-Failure occured during writing sensor-data to database." << endl;
+        }
+
+        // write forecast / webcontent values to database
+        dbi.writeToDataBase(ForecastValuesBuffer);
+        dbFailure = dbi.getDBFailure();
+        if (!dbFailure) {
+            log << SLevel(INFO) << "Succesfully wrote forecast-data to database." << endl;
+        } else {
+            log << SLevel(ERROR) << "DB-Failure occured during writing forecast-data to database." << endl;
+        }
+
+    }
+
+
 
     return 0;
 }
